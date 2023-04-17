cc92ad05db57e57310c2b0d1b5dd0b31361718e5
HassanHeydariNasab <hsn6@tuta.io>
2023-04-17 09:23:19 +0330

    Add booking button

--- src/components/BusinessShortInfo.svelte
+++ src/components/BusinessShortInfo.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
+
 	import { formattedDistance } from '$lib/format';
 	import type { Business } from '@type/business';
+	import Button from './Button.svelte';
 
 	export let business: Business;
 </script>
 
-<article class="text-center" dir="rtl">
+<article class="text-center p-4" dir="rtl">
 	<h2 class="font-bold text-lg">
 		{business.name}
 	</h2>
 	<div>{formattedDistance(+(business?.dist || 0))}</div>
 	<address>{business.phone}</address>
+	<Button>{$_('booking')}</Button>
 </article>
---
+++ src/components/Button.svelte
@@ -0,0 +1,3 @@
+<button class="bg-amber-500 rounded-md text-white font-bold px-4 py-2" {...$$props}>
+	<slot />
+</button>
