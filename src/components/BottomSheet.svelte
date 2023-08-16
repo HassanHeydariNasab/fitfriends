540d056302bbd3a04cf8352cafabac7fbaae9737
HassanHeydariNasab <hsn6@tuta.io>
2023-08-16 12:52:02 +0330

    Upgrade to Svelte 4

--- src/components/BottomSheet.svelte
+++ src/components/BottomSheet.svelte
@@ -12,8 +12,8 @@
 
 <div
 	class="absolute bottom-0 z-10 flex max-h-[100%] w-full flex-col overflow-y-auto rounded-t-3xl bg-gradient-to-tr from-pink-200 to-violet-200"
-	in:slide
-	out:slide
+	in:slide|global
+	out:slide|global
 >
 	<div class="m-4 cursor-pointer self-end" on:click={close}>
 		<CloseCircle class="text-gray-700 transition-colors hover:text-gray-900" />
