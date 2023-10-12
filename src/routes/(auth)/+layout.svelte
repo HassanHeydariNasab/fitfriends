f762ee6b8666e82d38a943e5bfbbe381ab009387
HassanHeydariNasab <hsn6@tuta.io>
2023-10-12 23:23:26 +0330

    Fix auth layout

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -3,9 +3,7 @@
 	import SelectLanguage from '@components/SelectLanguage.svelte';
 </script>
 
-<div class="relative flex min-h-full flex-col items-center overflow-y-auto">
-	<div class="flex-1" />
-	<slot />
-	<SelectLanguage buttonClass="text opacity-60" />
-	<div class="flex-1" />
-</div>
+<div class="flex-1" />
+<slot />
+<SelectLanguage buttonClass="text opacity-60" containerClass="self-center" />
+<div class="flex-1" />
