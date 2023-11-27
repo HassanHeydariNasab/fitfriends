e39ab3b767199a5d02a9dd560ec0e16775aeb551
HassanHeydariNasab <hsn6@tuta.io>
2023-11-27 14:11:30 +0330

    Fix auth test

--- be/src/auth/auth.service.spec.ts
+++ be/src/auth/auth.service.spec.ts
@@ -1,37 +1,26 @@
 import { env } from 'process';
 
 import { Test, TestingModule } from '@nestjs/testing';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-
-import { AuthService } from './auth.service';
-import { AUTH_OPTIONS } from './auth.options';
-import { JwtAccessStrategy } from './jwt-access.strategy';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from './auth.module';
+import { AuthService } from './auth.service';
+
 describe('AuthService', () => {
-  let service: AuthService;
   let module: TestingModule;
 
+  let service: AuthService;
+
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({ envFilePath: '.env.test', isGlobal: true }),
-        PassportModule,
-        JwtModule.register({}),
-      ],
-      providers: [
-        JwtAccessStrategy,
-        AuthService,
-        {
-          provide: AUTH_OPTIONS,
-          useValue: {
-            accessTokenSecret: env.ACCESS_TOKEN_SECRET,
-            refreshTokenSecret: env.REFRESH_TOKEN_SECRET,
-            accessTokenExpiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
-            refreshTokenExpiresIn: env.REFRESH_TOKEN_EXPIRES_IN,
-          },
-        },
+        AuthModule.forRoot({
+          accessTokenSecret: env.ACCESS_TOKEN_SECRET,
+          refreshTokenSecret: env.REFRESH_TOKEN_SECRET,
+          accessTokenExpiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
+          refreshTokenExpiresIn: env.REFRESH_TOKEN_EXPIRES_IN,
+        }),
       ],
     }).compile();
 
